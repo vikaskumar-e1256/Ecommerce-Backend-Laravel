efb014ff7f2c4da464e7f5c9638889d084e24621
Nishant <vikaskumar.e1256@gmail.com>
2023-11-03 12:03:38 +0530

    refactor authcontroller code

---
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use JWTAuth;
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request)
+    {
+        // Validate the data using Laravel validation rules.
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6|max:50',
+        ]);
+
+        // Check for validation errors.
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()], Response::HTTP_BAD_REQUEST);
+        }
+
+        // Create a new user.
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password), // Hash the password
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User created successfully',
+            'data' => $user,
+        ], Response::HTTP_OK);
+    }
+
+    public function signin(Request $request)
+    {
+        // Validate the credentials.
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6|max:50',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()], Response::HTTP_BAD_REQUEST);
+        }
+
+        // Attempt to create a JWT token.
+        try {
+            if (!$token = JWTAuth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Login credentials are invalid.',
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Could not create a token.',
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // Token created, return success response and JWT token.
+        return response()->json([
+            'success' => true,
+            'token' => $token,
+        ], Response::HTTP_OK);
+    }
+
+    public function signout(Request $request)
+    {
+        // Validate the request.
+        $validator = Validator::make($request->all(), [
+            'token' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()], Response::HTTP_BAD_REQUEST);
+        }
+
+        // Invalidate the JWT token.
+        try {
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User has been logged out',
+            ], Response::HTTP_OK);
+        } catch (JWTException $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out',
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\Auth\AuthController;
 use App\Http\Controllers\API\UserProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
